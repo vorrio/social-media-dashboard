0b13b06cb748e356835abdb6c21ceac6b60936ae
vorio <voriodev@gmail.com>
2023-10-06 18:34:34 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let currentThemeLight = localStorage.getItem("theme");
+const checkbox = document.querySelector('.switch-checkbox[name="theme"]');
+
+console.log(currentThemeLight);
+
+if (currentThemeLight === null) localStorage.setItem("theme", "dark");
+if (currentThemeLight === "light") {
+	checkbox.checked = "light";
+	document.querySelector("html").setAttribute("data-theme", "light");
+}
+
+checkbox.addEventListener("change", function() {
+	if (this.checked) {
+		currentThemeLight = "light";
+  	document.querySelector("html").setAttribute("data-theme", "light");
+  	localStorage.setItem("theme", "light");
+	} else {
+		currentThemeLight = "dark";
+  	document.querySelector("html").setAttribute("data-theme", "dark");
+  	localStorage.setItem("theme", "dark");
+	}
+})
